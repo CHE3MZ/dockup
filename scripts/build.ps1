858cccaf44c0b041e963f184b725f8fd2d4bd245
CHE3MZ <deadbylights@gmail.com>
2026-09-19 02:08:45 +0300

    e2e: foreground run, revert cycle, version stamping

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -4,5 +4,6 @@ if ($env:OS -notlike '*Windows*') { throw 'build.ps1 is Windows-only' }
 $env:GOOS = 'windows'
 $env:GOARCH = 'amd64'
 go vet ./...
-go build -o dockup.exe ./cmd/dockup
+$sha = git rev-parse --short HEAD
+go build -ldflags "-X main.version=v0.1.0-dev -X main.commit=$sha" -o dockup.exe ./cmd/dockup
 Write-Host 'built dockup.exe'
